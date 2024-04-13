6ff67d68885fba4afe306331d1843469f7f9c5d9
Poojitha Pulipati <pulipatipoojitha@gmail.com>
2024-04-13 10:31:24 +0530

    Logging

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setuptools.setup(
     author = AUTHOR_USER_NAME ,
     author_email = AUTHOR_EMAIL,
     description = "A small python package for NLP App",
-    long_description = long description,
+    long_description = "long description",
     url= f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     project_urls={
         "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
